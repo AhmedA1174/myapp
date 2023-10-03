212ee70bd68a69e5c208f9c567d0630da06ccede
ahmed <ahmed.abdi1174@outlook.com>
2023-10-03 14:18:44 +0100

    dynamic routes

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -1,27 +1,27 @@
+// App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Change Switch to Routes
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 
-// Update the import paths
 import Home from './pages/Home/Home';
 import Signup from './pages/Signup/Signup';
 import Login from './pages/Login/Login';
 import Dashboard from './pages/Dashboard/Dashboard';
+import PolicyDetail from './pages/Dashboard/PolicyDetail/PolicyDetail';
+import TestEditor from './pages/Dashboard/PolicyDetail/TestEditor/TestEditor';
 
 function App() {
   return (
     <Router>
       <div className="App">
-        {/* You can add a Navbar component here if you have one */}
-        <Routes>  {/* Change Switch to Routes */}
-          {/* Update the way you use Route */}
+        <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/login" element={<Login />} />
           <Route path="/dashboard" element={<Dashboard />} />
-          {/* Add more routes as needed */}
+          <Route path="/dashboard/:policyId" element={<PolicyDetail />} />
+          <Route path="/dashboard/:policyId/:testId" element={<TestEditor />} />
         </Routes>
-        {/* You can add a Footer component here if you have one */}
       </div>
     </Router>
   );
