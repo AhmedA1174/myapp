7fe60be1bfb5deb0e430f5ea4405499932ea8f52
ahmed <ahmed.abdi1174@outlook.com>
2023-10-03 14:07:42 +0100

    adding pages part 1

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -1,25 +1,26 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Change Switch to Routes
 import './App.css';
 
-// Import your page components
-import Home from './pages/Home';
-import Signup from './pages/Signup';
-import Login from './pages/Login';
-import Dashboard from './pages/Dashboard';
+// Update the import paths
+import Home from './pages/Home/Home';
+import Signup from './pages/Signup/Signup';
+import Login from './pages/Login/Login';
+import Dashboard from './pages/Dashboard/Dashboard';
 
 function App() {
   return (
     <Router>
       <div className="App">
         {/* You can add a Navbar component here if you have one */}
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/signup" component={Signup} />
-          <Route path="/login" component={Login} />
-          <Route path="/dashboard" component={Dashboard} />
+        <Routes>  {/* Change Switch to Routes */}
+          {/* Update the way you use Route */}
+          <Route path="/" element={<Home />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/dashboard" element={<Dashboard />} />
           {/* Add more routes as needed */}
-        </Switch>
+        </Routes>
         {/* You can add a Footer component here if you have one */}
       </div>
     </Router>
